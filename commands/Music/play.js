f981fe0cba7e5801e7efbaf1caaa1ae37265bf5a
Dark <52045774+andrihermawan1997@users.noreply.github.com>
2021-11-10 12:30:31 +0700

    Update play.js

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -51,7 +51,7 @@ module.exports = {
 					embeds: [new MessageEmbed()
 						.setColor(ee.wrongcolor)
 						.setFooter(ee.footertext, ee.footericon)
-						.setTitle(`<:declined:780403017160982538> Your Voice Channel is full, I can't join!`)
+						.setTitle(`:x: Your Voice Channel is full, I can't join!`)
 					],
 				});
 			if (channel.guild.me.voice.channel && channel.guild.me.voice.channel.id != channel.id) {
@@ -59,7 +59,7 @@ module.exports = {
 					embeds: [new MessageEmbed()
 						.setColor(ee.wrongcolor)
 						.setFooter(ee.footertext, ee.footericon)
-						.setTitle(`<:declined:780403017160982538> I am already connected somewhere else`)
+						.setTitle(`:x: I am already connected somewhere else`)
 					],
 				});
 			}
